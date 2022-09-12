08bcfa35e382a80680c466bd02de12dea4ec0da7
Charon116 <lcquang116@gmail.com>
2022-09-12 20:16:13 +0700

    update

--- src/models/index.js
+++ src/models/index.js
@@ -10,9 +10,9 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
+    sequelize = new Sequelize('dardbkepg8the', 'noqcttyfkbtyhg', 'e9a0534c467a22a035aaddeeb9fff0bbf948f53b915188957ca1d16fc0eafa54', {
+        host: 'ec2-54-86-106-48.compute-1.amazonaws.com',
+        dialect: 'postgres',
         operatorsAliases: 0,
         dialectOptions: {
             ssl : {
